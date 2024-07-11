587414be30eb938c64413e89496113ca4b815f98
victor <torvicv@gmail.com>
2024-07-11 18:24:27 +0200

    magic mouse funcionando

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
@@ -9,4 +10,5 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allow your team to quickly build robust real-time web applications.
  */
 
+import './magicmouse.js';
 import './echo';
--- resources/views/livewire/toggle.blade.php
+++ resources/views/livewire/toggle.blade.php
@@ -4,12 +4,24 @@ use Livewire\Volt\Component;
 use Illuminate\Support\Facades\Cache;
 use App\Events\SwitchFlipped;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Locked;
 
 new class extends Component {
     public $toggleSwitch = false;
 
+    #[Locked]
+    public $userId;
+
+    public $mousePositions = [];
+
     public function mount() {
 
+        if (!Session::has('user_id')) {
+            $this->userId = uniqid('user_', true);
+            Session::put('user_id', $this->userId);
+        } else {
+            $this->userId = Session::get('user_id');
+        }
         $this->toggleSwitch = Cache::get('toggleSwitch', false);
     }
 
@@ -23,17 +35,34 @@ new class extends Component {
         $this->toggleSwitch = $payload['toggleSwitch'];
         Cache::forever('toggleSwitch', $this->toggleSwitch);
     }
+
+    public function moveMouse($position) {
+        $this->mousePositions[] = $position;
+    }
 }; ?>
 
-<div wire:poll.500ms class="flex items-center justify-center min-h-screen" x-data="{ localToggle: @entangle('toggleSwitch') }">
-    <label for="toggleSwitch" class="flex items-center cursor-pointer">
-        <div class="relative">
-            <input type="checkbox" id="toggleSwitch" class="sr-only" x-model="localToggle"
-                wire:change="flipSwitch" />
-            <div class="block h-8 bg-gray-600 rounded-full w-14"></div>
-            <div class="absolute w-6 h-6 transition-transform duration-200 rounded-full left-1 top-1"
-                x-bind:class="{ 'translate-x-full bg-green-400': localToggle, 'bg-white': !localToggle }">
+<div
+    x-data="{
+    localToggle: @entangle('toggleSwitch'),
+    cursors: @entangle('mousePositions'),
+    }">
+    <div class="flex items-center justify-center min-h-screen">
+        <label for="toggleSwitch" class="flex items-center cursor-pointer">
+            <div class="relative">
+                <input type="checkbox" id="toggleSwitch" class="sr-only" x-model="localToggle"
+                    wire:change="flipSwitch" />
+                <div class="block h-8 bg-gray-600 rounded-full w-14"></div>
+                <div class="absolute w-6 h-6 transition-transform duration-200 rounded-full left-1 top-1"
+                    x-bind:class="{ 'translate-x-full bg-green-400': localToggle, 'bg-white': !localToggle }">
+                </div>
             </div>
+        </label>
+    </div>
+    <template x-for="cursor in cursors" :key="cursor.id">
+        <div class="absolute w-2 h-2 rounded-full bg-red-500"
+            x-bind:style="'left: ' + cursor.x + 'px; top: ' + cursor.y + 'px;'"
+        >
+
         </div>
-    </label>
+    </template>
 </div>
